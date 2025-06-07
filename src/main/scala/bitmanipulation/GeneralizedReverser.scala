469cde80a08849174d47b662153321ba9c89dd60
Ngoc Viet Anh Tu <ngtu00001@stud.uni-saarland.de>
2025-06-08 01:40:54 +0200

    Generalized Reverser logic

--- src/main/scala/bitmanipulation/GeneralizedReverser.scala
+++ src/main/scala/bitmanipulation/GeneralizedReverser.scala
@@ -14,6 +14,32 @@ abstract class AbstractGeneralizedReverser(bitWidth: Int) extends Module {
 class GeneralizedReverser(bitWidth: Int)
     extends AbstractGeneralizedReverser(bitWidth) {
 
-  ??? // TODO: implement Task 1.2 here
+  val patternBits = log2Ceil(bitWidth)
+  var currentResult = io.input
+  
+  // Apply reversal levels based on pattern bits
+  for (level <- 0 until patternBits) {
+    val swapDistance = 1 << level  // 2^level
+    val nextResult = Wire(UInt(bitWidth.W))
+    val resultBits = Wire(Vec(bitWidth, Bool()))
+    val currentBits = VecInit(currentResult.asBools)
+    
+    // For each bit position, decide whether to swap or keep
+    for (i <- 0 until bitWidth) {
+      val swapPartner = i ^ swapDistance
+      val shouldSwap = io.pattern(level)
+      
+      when(shouldSwap && (swapPartner.U < bitWidth.U)) {
+        resultBits(i) := currentBits(swapPartner)
+      }.otherwise {
+        resultBits(i) := currentBits(i)
+      }
+    }
+    
+    nextResult := resultBits.asUInt
+    currentResult = nextResult
+  }
+  
+  io.result := currentResult
 
 }
--- src/main/scala/bitmanipulation/LeadingZerosCounter.scala
+++ src/main/scala/bitmanipulation/LeadingZerosCounter.scala
@@ -13,62 +13,36 @@ abstract class AbstractLeadingZerosCounter(bitWidth: Int) extends Module {
 // You may expect bitWidth to be a power of two.
 class LeadingZerosCounter(bitWidth: Int)
     extends AbstractLeadingZerosCounter(bitWidth) {
+
+  // Recursive function to count leading zeros with logarithmic depth
+  def countLeadingZeros(input: UInt, width: Int): UInt = {
+    if (width == 1) {
+      // Base case: single bit
+      // If bit is 0, then 1 leading zero, else 0 leading zeros
+      Mux(input === 0.U, 1.U, 0.U)
+      //Mux(condition, trueValue, falseValue)
+    } else {
+      // Recursive case: divide and conquer
+      val halfWidth = width / 2
+      val leftHalf = input(width - 1, halfWidth)
+      val rightHalf = input(halfWidth - 1, 0)
       
-    var accumulatedCount = 0.U(log2Ceil(bitWidth + 1).W)
-    var currentFragment = io.input // The part of input we are currently examining
-
-    var currentProcessingWidth = bitWidth // The width of the current fragment being processed
-
-    // The loop run log2(bitWidth) times
-    // For bitWidth = 1, log2Ceil(1) = 0 , loop does not run
-    // For bitWidth = 2, log2Ceil(2) = 1 , loop runs once
-    // For bitWidth = 32 , log2Ceil(32) = 5, loop runs 5 times
-    for (_ <- 0 until log2Ceil(bitWidth)) {
-    // stepChunkSize is the size of the upper/lower half of the currentFragment
-    // (and also the amount to add to the count if the upper half is all zeros)
-    val stepChunkSize = currentProcessingWidth / 2
-
-    // Extract the upper and lower halves of the currentFragment
-    // Note: currentFragment itself shrinks in width through Mux selection below
-    val upperHalf = currentFragment(currentProcessingWidth - 1, stepChunkSize)
-    val lowerHalf = currentFragment(stepChunkSize - 1, 0)
-
-    val upperHalfIsZero = (upperHalf === 0.U)
-
-    // Temporary variables to hold the next state for this stage
-    val nextAccumulatedCount = Wire(UInt(log2Ceil(bitWidth + 1).W))
-    val nextFragment = Wire(UInt(stepChunkSize.W)) // The next fragment will be half the width
-
-    when(upperHalfIsZero) {
-      nextAccumulatedCount := accumulatedCount + stepChunkSize.U
-      nextFragment := lowerHalf
-    }.otherwise {
-      nextAccumulatedCount := accumulatedCount
-      nextFragment := upperHalf
+      // Recursively count leading zeros in each half
+      val leftCount = countLeadingZeros(leftHalf, halfWidth)
+      val rightCount = countLeadingZeros(rightHalf, halfWidth)
+      
+      // Check if left half is all zeros
+      val leftAllZeros = leftHalf === 0.U
+      
+      // Combine results:
+      // If left half has any 1-bits -> result is just left count
+      // If left half is all zeros -> result is left_width + right count
+      val resultWidth = log2Ceil(width + 1)
+      Mux(leftAllZeros, (halfWidth.U(resultWidth.W) + rightCount), leftCount)
     }
-
-    // Update the Scala vars for the next iteration (i.e., for generating the next stage of hardware)
-    accumulatedCount = nextAccumulatedCount
-    currentFragment = nextFragment // currentFragment is now effectively narrower
-    currentProcessingWidth = stepChunkSize
   }
+  
+  // Connect the recursive function to the output
+  io.result := countLeadingZeros(io.input, bitWidth)
 
-  // After the loop:
-  // - If bitWidth = 1, loop didn't run. accCount = 0, currentFragment = io.input (1 bit).
-  // - If bitWidth > 1, loop ran. currentFragment is now 1-bit wide.
-  //   accumulatedCount has the count of leading zeros from all preceding (wider) zero-blocks.
-  //
-  // The final 1-bit currentFragment determines the last bit of the count.
-  // If this last processed bit (currentFragment(0)) is 0, it means it's also a leading zero.
-  // Example: input 0b0000 (width 4)
-  // Iteration 1 (width=4, step=2): upper="00", lower="00". acc=2. fragment="00" (2-bit).
-  // Iteration 2 (width=2, step=1): upper="0", lower="0". acc=2+1=3. fragment="0" (1-bit).
-  // Loop ends. acc=3. fragment="0". We need to add 1 because fragment(0) is 0. Result = 4.
-  //
-  // Example: input 0b0001 (width 4)
-  // Iteration 1 (width=4, step=2): upper="00", lower="01". acc=2. fragment="01" (2-bit).
-  // Iteration 2 (width=2, step=1): upper="0", lower="1". acc=2+1=3. fragment="1" (1-bit).
-  // Loop ends. acc=3. fragment="1". We add 0 because fragment(0) is 1. Result = 3.
-
-  io.result := accumulatedCount + (currentFragment(0) === 0.U).asUInt
 }
